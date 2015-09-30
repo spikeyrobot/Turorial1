419dfed78f340535928a310ef6662010c919d6ed
Campbell Crowley <mail@campbellcrowley.com>
2015-09-29 20:53:47 -0700

    Added /nightvision
    Added /nightvision (/nv) command, cleaned up config file
    Added permissions for /nightvision (spikeycraft.nightvision.command)

--- src/me/spikeyrobot/SpikeyCraft.java
+++ src/me/spikeyrobot/SpikeyCraft.java
@@ -23,6 +23,8 @@ import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.bukkit.event.player.PlayerLoginEvent;
 import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
 
 
 public class SpikeyCraft extends JavaPlugin implements Listener{
@@ -31,8 +33,12 @@ public class SpikeyCraft extends JavaPlugin implements Listener{
 	public	Map<String, Integer> 	xCoords	= new HashMap<String, Integer>();
 	public	Map<String, Integer> 	yCoords	= new HashMap<String, Integer>();
 	public	Map<String, Integer> 	zCoords	= new HashMap<String, Integer>();
-	public static String OwnerName = "spikeyrobot";
 	
+	public static String OwnerName = "spikeyrobot";
+	public static String chatPrefix = "§a[SpikeyCraft]: ";
+	
+	private final String permPrefix = "spikeycraft.";
+
 	public ArrayList<String> BRBList = new ArrayList<String>();
 	
 	@Override
@@ -41,6 +47,7 @@ public class SpikeyCraft extends JavaPlugin implements Listener{
 		saveDefaultConfig();
 		
 		OwnerName = getConfig().getString("OPMEOwner");
+		chatPrefix = ChatColor.translateAlternateColorCodes('&', getConfig().getString("Prefix"));
 		
 		getServer().getPluginManager().registerEvents(this, this);
 	}
@@ -53,8 +60,9 @@ public class SpikeyCraft extends JavaPlugin implements Listener{
 	public void reload() {
 		
 		saveDefaultConfig();
-		
+
 		OwnerName = getConfig().getString("OPMEOwner");
+		chatPrefix = getConfig().getString("Prefix");
 	}
 	
 	@SuppressWarnings("deprecation")
@@ -108,7 +116,7 @@ public class SpikeyCraft extends JavaPlugin implements Listener{
 		Player player = (Player) sender;
 		
 		//toggle brb off
-		if(findBRB(player.getName()) != -1 && getConfig().getBoolean("EnableBRB") && !cmd.getName().equalsIgnoreCase("brb")) {
+		if(find("brb", player.getName()) != -1 && getConfig().getBoolean("EnableBRB") && !cmd.getName().equalsIgnoreCase("brb")) {
 			brb(player);
 		}
 		
@@ -117,13 +125,25 @@ public class SpikeyCraft extends JavaPlugin implements Listener{
 		} else if(cmd.getName().equalsIgnoreCase("opme") && sender instanceof Player && player.getName().equals(OwnerName) && getConfig().getString("EnableOPME").equalsIgnoreCase("true")) {
 			new OwnerJoin(OwnerName, this);
 			return true;
-		} else if(
-				(
-						cmd.getName().equalsIgnoreCase("opme")
-					 || cmd.getName().equalsIgnoreCase("brb")
-				) 
-				&& sender instanceof Player
-				) {
+		} else if ((
+					   cmd.getName().equalsIgnoreCase("nv")
+					|| cmd.getName().equalsIgnoreCase("nightvision"))
+					&& sender instanceof Player
+					&& player.hasPermission(permPrefix + "nightvision.command")
+					&& getConfig().getBoolean("EnableNV")
+					) {
+			return (find("nv", player.getName())==1);
+		} else if(((
+				   cmd.getName().equalsIgnoreCase("nv")
+				|| cmd.getName().equalsIgnoreCase("nightvision"))
+				&& getConfig().getBoolean("EnableNV")
+				 ) && sender instanceof Player){
+			player.sendMessage(chatPrefix + "" + ChatColor.RED + "You do not have permission to do this command. If you wish to have this command, you may purchase it with /buy");
+			return true;
+		} else if((
+				   cmd.getName().equalsIgnoreCase("opme")
+				|| cmd.getName().equalsIgnoreCase("brb")
+			) && sender instanceof Player) {
 			
 			player.sendMessage("Unknown command. Type \"/?\" for help.");
 			
@@ -134,11 +154,11 @@ public class SpikeyCraft extends JavaPlugin implements Listener{
 		return false;
 		
 	}
-	
+
 	@EventHandler(priority = EventPriority.HIGHEST)
 	public void onPlayerChat(AsyncPlayerChatEvent event)
 	{
-		if(findBRB(event.getPlayer().getName()) != -1) {
+		if(find("brb", event.getPlayer().getName()) != -1) {
 			brb(event.getPlayer());
 		}
 		return;
@@ -148,7 +168,7 @@ public class SpikeyCraft extends JavaPlugin implements Listener{
 	public void onPlayerMove(PlayerMoveEvent event) {
 		if(event.getFrom() != event.getTo()) {
 			
-			if(getConfig().getBoolean("EnableBRB") && findBRB(event.getPlayer().getName()) != -1) {
+			if(getConfig().getBoolean("EnableBRB") && find("brb", event.getPlayer().getName()) != -1) {
 				brb(event.getPlayer());
 			}
 			
@@ -160,23 +180,42 @@ public class SpikeyCraft extends JavaPlugin implements Listener{
 	 */
 	private boolean brb(Player player) {
 		
-		if(findBRB(player.getName()) == -1) {
+		if(find("brb", player.getName()) == -1) {
 			Bukkit.getServer().broadcastMessage("§7* " + player.getDisplayName() + " §7" + getConfig().getString("BRBMessage"));
 			BRBList.add(player.getName());
 		} else {
 			Bukkit.getServer().broadcastMessage("§7* " + player.getDisplayName() + " §7" + getConfig().getString("BackMessage"));
-			BRBList.remove(findBRB(player.getName()));
+			BRBList.remove(find("brb", player.getName()));
 		}
 		return true;
 	}
 	/**
 	 * @return
 	 */
-	private int findBRB(String playername) {
-		for(int i=0; i<BRBList.size(); i++) {
-			if(playername.equals(BRBList.get(i))) {
-				return i;
+	private int find(String type, String playername) {
+		
+		if(type.equals("brb")){
+			for(int i=0; i<BRBList.size(); i++) {
+				if(playername.equals(BRBList.get(i))) {
+					return i;
+				}
 			}
+		} else if (type.equals("nv")) {
+			PotionEffectType nightvision = PotionEffectType.NIGHT_VISION;
+			Player player = Bukkit.getPlayer(playername).getPlayer();
+				
+			for(PotionEffect pe: player.getActivePotionEffects()) {
+				if(pe.getType().equals(nightvision)) {
+		            player.removePotionEffect(nightvision);
+					player.sendMessage(chatPrefix + ChatColor.GREEN + getConfig().getString("NoNVMessage"));
+		            return 1;
+				}
+			}
+			if (player != null) {
+				player.addPotionEffect(new PotionEffect(nightvision,9999999,0,true));
+				player.sendMessage(chatPrefix + ChatColor.GREEN + getConfig().getString("NVMessage"));
+				return 1;
+	        }
 		}
 		return -1;
 	}
@@ -197,4 +236,5 @@ public class SpikeyCraft extends JavaPlugin implements Listener{
 		if(event.getPlayer().getName().equals(OwnerName) && getConfig().getBoolean("AnnounceOwnerLeave"))
 			Bukkit.broadcastMessage(ChatColor.RED + " " + getConfig().getString("AnnounceLeaveMessage"));
 	}
+	
 }
